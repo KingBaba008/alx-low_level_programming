f078eacb8b555debb2c322d8e2d416aff80e6910
KingBaba008 <vicky.chairman@gmail.com>
2022-10-15 14:10:06 -0700

    My C solutions

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,7 +3,7 @@
  * main - print if the number is postive, zero, or negative
  * Description: using the main function
  * this program prints "Programming is positive, zero, or negative
- * Return: 0
+ * Return: Always 0
  */
 int main(void)
 {
