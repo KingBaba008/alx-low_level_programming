ee28b2deecc62150c5821063028d5a667f4cbcca
KingBaba008 <vicky.chairman@gmail.com>
2022-11-16 15:53:51 -0800

    3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,32 +1,39 @@
-#ifndef CALC_H
+#include "3-calc.h"
 
-#define CALC_H
+#include <stdio.h>
+
+#include <stdlib.h>
 /**
- * struct op - Struct op
- * @op: The operator
- * @f: The function associated
+ * main - Prints the result of simple operations.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ * Return: Always 0.
  */
 
-typedef struct op
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
+	int num1, num2;
 	char *op;
 
-	int (*f)(int a, int b);
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	num1 = atoi(argv[1]);
+	op = argv[2];
+	num2 = atoi(argv[3]);
+
+	if (get_op_func(op) == NULL || op[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", get_op_func(op)(num1, num2));
+	return (0);
 }
-op_t;
-
-int op_add(int a, int b);
-
-int op_sub(int a, int b);
-
-int op_mul(int a, int b);
-
-int op_div(int a, int b);
-
-int op_mod(int a, int b);
-
-int (*get_op_func(char *s))(int, int);
-
-
-
-#endif
